bd4a8f4e8d22dcee757147caaf48080921a26a94
hugodr <hdurand777@gmail.com>
2016-06-23 15:26:16 +0200

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -78,17 +78,18 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 
 // write - invoke function to write key/value pair
 func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	var name, value string
+	var name, value1 string, value2 string
 	var err error
 	fmt.Println("running write()")
 
-	if len(args) != 2 {
+	if len(args) != 3 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
 	}
 
 	name = args[0]                            //rename for funsies
-	value = args[1]
-	err = stub.PutState(name, []byte(value))//write the variable into the chaincode state
+	value1 = args[1]
+	value2 = args[2]
+	err = stub.PutState(name, []byte(value1), []byte(value2))//write the variable into the chaincode state
 	if err != nil {
 		return nil, err
 	}
